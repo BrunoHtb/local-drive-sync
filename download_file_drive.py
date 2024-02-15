7adc74c0d55840a79a6e35731b7045cc1102c513
bruno taborda <bruno.taborda1711@gmail.com>
2024-02-14 22:56:28 -0300

    melhorando funcao upload e tentativa de delete

--- download_file_drive.py
+++ download_file_drive.py
@@ -27,6 +27,14 @@ def list_file_in_folder(service, id_folder):
     files = response.get("files", [])
     return files
 
+def file_exists_in_folder(service, folder_id, file_name):
+    results = service.files().list(
+        q=f"'{folder_id}' in parents and name='{file_name}'",
+        fields="files(id, name)"
+    ).execute()
+    files = results.get('files', [])
+    return len(files) > 0
+
 def download_file(service, file_id, folder_target):
     file_info = service.files().get(fileId=file_id).execute()
 
@@ -47,24 +55,39 @@ def download_file(service, file_id, folder_target):
 
 def upload_file(service, local_file_path, folder_id):
     file_name = os.path.basename(local_file_path)
-    media = MediaFileUpload(local_file_path, resumable=True)
-    request = service.files().create(
-        media_body=media,
-        body={
-            'name': file_name,
-            'parents': [folder_id],
-        }
-    )
-    response = None
-    while response is None:
-        status, response = request.next_chunk()
-        if status:
-            print(f"Progresso do upload: {int(status.progress() * 100)}%")
-    print(f"Upload concluído para: {file_name}")
+    file_exists = file_exists_in_folder(service, folder_id, file_name)
 
-def verificar_novos_arquivos_local(local_folder_path):
-    # Obtém a lista de arquivos no diretório local
-    local_files = [os.path.join(local_folder_path, arquivo) for arquivo in os.listdir(local_folder_path) if os.path.isfile(os.path.join(local_folder_path, arquivo))]
+    if not file_exists:
+        media = MediaFileUpload(local_file_path, resumable=True)
+        request = service.files().create(
+            media_body=media,
+            body={
+                'name': file_name,
+                'parents': [folder_id],
+            }
+        )
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
+            if status:
+                print(f"Progresso do upload: {int(status.progress() * 100)}%")
+        print(f"Upload concluído para: {file_name}")
+    else:
+        print(f"Arquivo '{file_name}' já existe no diretório. Upload ignorado.")
+
+def delete_file(service, folder_id, file_name):
+    file_in_folder_to_delete = list_file_in_folder(service, folder_id)
+    
+    for file in file_in_folder_to_delete:
+        if file.get("name") == file_name:
+            file_id = file['id']
+            if folder_id:
+                body_value = {'trashed': True}
+                service.files().update(fileId=file_id, body=body_value).execute()
+            print(f"Arquivo '{file_name}' excluído do Google Drive.")
+ 
+def verify_new_file_local(local_folder_path):
+    local_files = [os.path.join(local_folder_path, file) for file in os.listdir(local_folder_path) if os.path.isfile(os.path.join(local_folder_path, file))]
     return local_files
 
 def main():
@@ -80,13 +103,15 @@ def main():
         
         for file in file_in_folder:
             folder_target = os.path.join("C:/Users/bruno/OneDrive/Imagens/Documentos/MicrostationConnection/SP_Fase2", file.get("name"))
-            download_file(service, file.get("id"), folder_target)
-            #print(f'Arquivo no diretório {folder_name_main}: {file.get("name")}, {file.get("id")}')
+            #download_file(service, file.get("id"), folder_target)
 
         local_folder_path = "C:/Users/bruno/OneDrive/Imagens/Documentos/MicrostationConnection/SP_Fase2/OK"
-        local_files = verificar_novos_arquivos_local(local_folder_path)
+        local_files = verify_new_file_local(local_folder_path)
+
         for local_file in local_files:
+            file_name = os.path.basename(local_file)
             upload_file(service, local_file, id_folder_ok)
+            #delete_file(service, id_folder_search, file_name)
     else:
         print(f'Diretório {folder_name_main} não encontrado no Google Drive.')
 
